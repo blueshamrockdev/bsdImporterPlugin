2c35d1f134956b8dc7a1ba45ba9f8c8c60da1b71
Micah Breedlove <druid628@gmail.com>
2011-12-27 22:43:26 -0600

    Initial Commit

---
+++ config/bsdImporterPluginConfiguration.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * bsdImporterPlugin configuration.
+ * 
+ * @package     bsdImporterPlugin
+ * @subpackage  config
+ * @author      druid628
+ * @version     SVN: $Id: PluginConfiguration.class.php 17207 2009-04-10 15:36:26Z Kris.Wallsmith $
+ */
+class bsdImporterPluginConfiguration extends sfPluginConfiguration
+{
+  const VERSION = '1.0.0-DEV';
+
+  /**
+   * @see sfPluginConfiguration
+   */
+  public function initialize()
+  {
+  }
+}
